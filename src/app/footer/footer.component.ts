8335b8347ee389086b7497e339b3b995c3e2b89d
jeremykailaederach <jeremy.kai.laederach@gmail.com>
2022-06-05 14:43:01 +0200

    adds footer links

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,21 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-  footerText: string = "";
+  navbarfixed: boolean = false;
 
   constructor() { }
-
-  ngOnInit(): void {
-    var time = new Date().getHours();
-    if (time < 11){
-      this.footerText = "Good morning";
+  @HostListener('window:scroll', ['$event']) onScroll(){
+    if (window.scrollY > 50){
+      this.navbarfixed = true;
     }
-    else if (time < 15){
-      this.footerText = "Good afternoon";
-    }
-    else if (time < 24){
-      this.footerText = "Good evening";
+    else {
+      this.navbarfixed = false;
     }
   }
 
+  ngOnInit(): void {
+  }
 }
