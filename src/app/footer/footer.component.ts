27f8b42e453c4e42b23cfb5aeaf7103512f0d867
jeremykailaederach <jeremy.kai.laederach@gmail.com>
2022-06-07 01:41:17 +0200

    making footer responsive

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,8 +7,15 @@ import { Component, HostListener, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   navbarfixed: boolean = false;
+  desktop: boolean = true;
+  phone: boolean = false;
+  public innerWidth: any;
 
   constructor() { }
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.innerWidth = window.innerWidth;
+  }
   @HostListener('window:scroll', ['$event']) onScroll(){
     if (window.scrollY > 50){
       this.navbarfixed = true;
@@ -18,6 +25,23 @@ export class FooterComponent implements OnInit {
     }
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.innerWidth = window.innerWidth;
+    if (this.innerWidth < 1400) {
+      this.hidedesktop();
+    }
+    if (this.innerWidth > 1400) {
+      this.showdesktop();
+    }
+  }
+
+  showdesktop() {
+    this.desktop = true;
+    this.phone = false;
+  }
+
+  hidedesktop() {
+    this.desktop = false;
+    this.phone = true;
   }
 }
